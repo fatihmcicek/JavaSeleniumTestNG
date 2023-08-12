cef2109c04aa4019522a21bfa91ebcf1d1a3cb65
Fatih Mehmet Çiçek <fatihmcicek5@gmail.com>
2023-08-12 23:45:39 +0300

    added extend reports

--- src/test/java/Utilities/BaseTest.java
+++ src/test/java/Utilities/BaseTest.java
@@ -1,5 +1,7 @@
 package Utilities;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,10 +10,18 @@ import org.testng.annotations.BeforeClass;
 
 public abstract class BaseTest {
     protected WebDriver driver;
+    private static ExtentSparkReporter sparkReporter;
+    private static ExtentReports extent;
+
     @BeforeClass
     public void Before(){
         System.setProperty("webdriver.http.factory", "jdk-http-client");
         WebDriverManager.chromedriver().setup();
+
+        extent = new ExtentReports();
+        sparkReporter = new ExtentSparkReporter(System.getProperty("user.dir") +"\\logs\\Reports.html");
+        extent.attachReporter(sparkReporter);
+
         driver= new ChromeDriver();
         driver.manage().window().maximize();
     }
